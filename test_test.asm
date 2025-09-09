8022446c2f106ba328c7169129245008484f2696
evjeesm <jekas.obps@gmail.com>
2025-09-09 18:28:49 +0300

    args in CCALL and SYSCALL are optional. use * to dereference a pointer

--- test_test.asm
+++ test_test.asm
@@ -16,13 +16,19 @@ _start:
     ASSERT_EQ rax, rdx, "Compare registers"
     ASSERT_EQ 70, 70, "Compare numerics"
     ASSERT_EQ rax, 70, "Register with Numeric"
-    CONSTANT_1 = 2001
+    CONSTANT_1 = 2000
     CONSTANT_2 = 2000
     ASSERT_EQ CONSTANT_1, CONSTANT_2, "CONSTANTS"
     ASSERT_NE CONSTANT_1, CONSTANT_2, "Not eq"
 
+    ;; CCALL assert_strcpy, [other], [src], 5, src_len
+    ;; CCALL assert_str_begin
+    ;; CCALL assert_str_build, [src], src_len
+    ;; PRINT [assert_str], [assert_str_index]
     SYSCALL EXIT, 0
 
 
 segment readable writable
-
+src db "Hello, World", 10
+src_len = $ - src
+other rb 100
