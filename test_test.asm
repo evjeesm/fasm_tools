fa946e68fead29e65e7f559bba1d7f96beddf39c
evjeesm <jekas.obps@gmail.com>
2025-09-13 15:17:34 +0300

    ZERO NOT_ZERO NEGATIVE POSITIVE; + refactor

--- test_test.asm
+++ test_test.asm
@@ -19,12 +19,16 @@ _start:
     CONSTANT_1 = 2000
     CONSTANT_2 = 2000
     ASSERT_EQ CONSTANT_1, CONSTANT_2, "CONSTANTS"
-    ASSERT_NE CONSTANT_1, CONSTANT_2, "Not eq"
+    ;; ASSERT_NE CONSTANT_1, CONSTANT_2, "Not eq"
 
     mov rax, 0xAAAAAAAA_AAAAAAAA
     mov rdx, 0xFFFFFFFF
     mov rax, rdx
     ASSERT_EQ rax, 0xFFFFFFFF, "Compare registers"
+
+    ASSERT_POSITIVE 1, "Should be positive"
+    ASSERT_ZERO 0, "Should be zero"
+    ASSERT_POSITIVE -1, "Should be positive"
 
     ;; CCALL assert_strcpy, [other], [src], 5, src_len
     ;; CCALL assert_str_begin
