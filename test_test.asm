eff678d68fad63f399133576c6e4530be3cca288
evjeesm <jekas.obps@gmail.com>
2025-09-19 21:21:43 +0300

    Refactoring (ranges are mostly clear); smol fixes

--- test_test.asm
+++ test_test.asm
@@ -21,24 +21,22 @@ _start:
     ASSERT_EQ CONSTANT_1, CONSTANT_2, "CONSTANTS"
     ;; ASSERT_NE CONSTANT_1, CONSTANT_2, "Not eq"
 
-    mov rax, 0xAAAAAAAA_AAAAAAAA
-    mov rdx, 0xFFFFFFFF
+    mov rax, 0xAAAA_AAAA_AAAA_AAAA
+    mov rdx, 0xFFFF_FFFF
     mov rax, rdx
-    ASSERT_EQ rax, 0xFFFFFFFF, "Compare registers"
+    ASSERT_EQ rax, 0xFFFF_FFFF, "Compare registers"
 
     ASSERT_POSITIVE 1, "Should be positive"
     ASSERT_ZERO 0, "Should be zero"
     ASSERT_POSITIVE +1, "Should be positive"
-    ASSERT_RANGES_BYTE_NE src, src, src_len, "Hoho"
-    ;; CCALL assert_strcpy, [other], [src], 5, src_len
-    ;; CCALL assert_str_begin
-    ;; CCALL assert_str_build, [src], src_len
-    ;; PRINT [assert_str], [assert_str_index]
+    ASSERT_RANGE_ALL_NE src, dest, src_len, "Hoho"
+    ASSERT_RANGE_CHAR_ALL_NE src, 'H', src_len, "Hoho"
+
     SYSCALL EXIT, 0
 
 
 segment readable writable
-src db "Hello, Borld", 10
+src  db "Hello, World", 10
 src_len = $ - src
 
 dest db "Hello, World", 10
