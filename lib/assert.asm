83b8f33015a7a11fc3dcb6865338a20181c47246
evjeesm <jekas.obps@gmail.com>
2025-10-05 15:57:29 +0300

    Single source library file support

--- lib/assert.asm
+++
@@ -1,5 +0,0 @@
-include 'comptime.inc'
-FORMAT_ELF64_OBJECT
-
-IMPL_FOR_DEBUG 'assert.inc'
-
--- lib/setjmp.asm
+++
@@ -1,5 +0,0 @@
-include 'comptime.inc' ;; requied
-FORMAT_ELF64_OBJECT    ;; sets BUILD_TYPE
-
-;; Implementation of the setjmp embedded right here
-IMPL 'setjmp.inc'
